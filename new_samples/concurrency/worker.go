8ee3e44f3d72df5a0b7f848f6a44adb3716dfb3e
“Kevin” <kevlar_ksb@yahoo.com>
2026-03-18 13:37:51 -0700

    nit: remove HelloWorld from worker template
    Signed-off-by: “Kevin” <kevlar_ksb@yahoo.com>

--- new_samples/concurrency/worker.go
+++ new_samples/concurrency/worker.go
@@ -43,7 +43,7 @@ func StartWorker() {
 		Domain,
 		TaskListName,
 		workerOptions)
-	// HelloWorld workflow registration
+	// workflow registration
 	w.RegisterWorkflowWithOptions(BatchWorkflow, workflow.RegisterOptions{Name: "cadence_samples.BatchWorkflow"})
 	w.RegisterActivityWithOptions(BatchActivity, activity.RegisterOptions{Name: "cadence_samples.BatchActivity"})
 
